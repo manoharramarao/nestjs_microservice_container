036bb6993daee330ceff75df80f07a70ee45d080
msr <manoharramarao@gmail.com>
2021-04-23 10:04:28 +0000

    created sample gRPC method

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
+import { GrpcMethod } from '@nestjs/microservices';
 import { AppService } from './app.service';
+import { ResDto } from './dto/res.dto';
 
 @Controller()
 export class AppController {
@@ -8,5 +10,12 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @GrpcMethod('nestjsService', 'callGrpc')
+  async callGrpc(): Promise<ResDto>{
+    let resDto = new ResDto();
+    resDto.res = "Response from microservice";
+    return resDto;
   }
 }
